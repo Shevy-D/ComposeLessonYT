32aaf59731c1e2f87bf543dfdef3324fa7219149
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-06-19 19:24:39 +0500

    lesson 5

--- app/src/main/java/com/shevy/composelessonyt/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyt/MainActivity.kt
@@ -1,93 +1,67 @@
 package com.shevy.composelessonyt
 
 import android.os.Bundle
-import android.util.Log
-import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.shevy.composelessonyt.ui.theme.ComposeLessonYTTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
-                listItem(name = "Tusha Kutusha", prof = "Actor")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
-
         }
     }
 }
 
 @Composable
-private fun listItem(name: String, prof: String) {
-    Card(
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .pointerInput(Unit){
-/*                detectDragGesturesAfterLongPress { change, dragAmount ->
-                    Log.d("MyLog", "Long press: $dragAmount")
-                }*/
-                detectTapGestures {
-                    Log.d("MyLog", "Long press: $it")
+            .size(200.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+                when (++counter.value) {
+                    10 -> color.value = Color.Green
+                    20 -> color.value = Color.Red
                 }
-            }
-/*            .clickable {
-                Log.d("MyLog", "Clicked")
-            }*/,
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
+            },
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                )
-                Column(modifier = Modifier.padding(start = 16.dp)) {
-                    Text(text = name)
-                    Text(text = prof)
-                }
-
-
-            }
-
-        }
-
+        Text(
+            text = counter.value.toString(),
+            color = Color.White,
+            fontSize = 30.sp
+        )
     }
 }
