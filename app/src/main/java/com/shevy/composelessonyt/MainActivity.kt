fca46c1d147d5b1535df59b76ce846cd3776aa32
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-06-19 17:46:26 +0500

    lesson 4

--- app/src/main/java/com/shevy/composelessonyt/MainActivity.kt
+++ app/src/main/java/com/shevy/composelessonyt/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.shevy.composelessonyt
 
 import android.os.Bundle
+import android.util.Log
 import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +31,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                listItem(name = "Tusha Kutusha", prof = "Actor")
+                listItem(name = "Tusha Kutusha", prof = "Actor")
+                listItem(name = "Tusha Kutusha", prof = "Actor")
+                listItem(name = "Tusha Kutusha", prof = "Actor")
+                listItem(name = "Tusha Kutusha", prof = "Actor")
+                listItem(name = "Tusha Kutusha", prof = "Actor")
                 listItem(name = "Tusha Kutusha", prof = "Actor")
                 listItem(name = "Tusha Kutusha", prof = "Actor")
                 listItem(name = "Tusha Kutusha", prof = "Actor")
@@ -43,7 +52,18 @@ private fun listItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .pointerInput(Unit){
+/*                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d("MyLog", "Long press: $dragAmount")
+                }*/
+                detectTapGestures {
+                    Log.d("MyLog", "Long press: $it")
+                }
+            }
+/*            .clickable {
+                Log.d("MyLog", "Clicked")
+            }*/,
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
